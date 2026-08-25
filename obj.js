f699670f60a09dc453eb85b5188f19092e8cb4bc
Tiran <tiranohanyan96@gmail.com>
2026-08-25 16:21:04 +0400

    Add array basic operations task

---
+++ obj.js
@@ -0,0 +1,13 @@
+let numbers =;
+
+
+
+let firstElement = numbers[0];
+let lastElement = numbers[numbers.length - 1];
+console.log(`Առաջին տարր: ${firstElement}, Վերջին տարր: ${lastElement}`);
+console.log(`Զանգվածի երկարությունը: ${numbers.length}`);
+
+
+numbers.push(10);
+
+console.log("Թարմացված զանգվածը:", numbers);
